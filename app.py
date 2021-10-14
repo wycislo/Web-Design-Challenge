4b05be6f168e01f44813a7bb5bf0dfb4a633bff5
wycislo <wycislo@yahoo.com>
2021-10-14 14:48:36 -0500

    navbar

--- app.py
+++ app.py
@@ -2,25 +2,7 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-posts = [
-
-    {
-        'author': 'Henry',
-        'title': 'DBA',
-    }
-]
-
-
-
 @app.route("/")
-def home():
-    return render_template('home.html', posts=posts, title='Home')
-
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
-
-@app.route("/index.html")
 def index():
     return render_template('index.html')
 
